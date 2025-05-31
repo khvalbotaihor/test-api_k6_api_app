ab254d4ce4027df114369ab35f3613b676169dd5
khvalbotaihor <31129714+khvalbotaihor@users.noreply.github.com>
2025-05-31 11:17:28 +0300

    add

--- http-get.js
+++ http-get.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
-import {check } from 'k6';
+import { check } from 'k6';
+import { randomItem } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 
 export default function () {
@@ -12,16 +13,26 @@ export default function () {
         'is content-type application/json': (r) => r.headers['Content-Type'] === 'application/json',
         'is response body not empty': (r) => r.body.length > 0,
     });
+console.log(`Response body of all crocodiles: ${res.body}`);
+    const crocodileIds = crocodiles.map(item => item.id)
 
-    const crocodileId = crocodiles[0].id;
-    const crocodileName = crocodiles[0].name;
+    const crocodileId = randomItem(crocodileIds);
+    console.log(`Randomly selected crocodile ID: ${crocodileId}`);
+    //const crocodileId = crocodiles[0].id;
+    console.log(`Crocodile IDs: ${crocodileIds}`);
+    
+    const crocodileName = crocodiles.filter(crocodile => crocodile.id === crocodileId)[0].name;
+    console.log(`Crocodile Name in     const crocodileName = crocodiles[crocodileId].name: ${crocodileName}`);
     res = http.get(`http://localhost:3001/public/crocodiles/${crocodileId}`);  
     console.log(`Response body: ${res.body}`);
     console.log(`header: ${res.headers['Content-Type']}`);
+    console.log(`Crocodile Name in ${res.json().name}: ${res.json().name}`);
 
     check(res, {
-        'is status 200 for crocodile 7': (r) => r.status === 200,
+        'is status 200 for crocodile': (r) => r.status === 200,
+        [`is is ${crocodileId} crocodile`]: (r) => r.json().id === crocodileId,
         [`name is ${crocodileName}`]: (r) => r.json().name === crocodileName,
+
     });
 
 }
